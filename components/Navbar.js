a5dc63ef286ef6614a2e742bfd480f44c3ed6ec7
Niels Louwes <niels.louwes@and.digital>
2022-06-27 16:51:58 +0200

    added global styling css reset

---
+++ components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Styled = styled.div`
+  height: 7vh;
+  border: 1px solid black;
+`;
+
+function Navbar() {
+  return (
+    <Styled>
+        
+
+    </Styled>
+  )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Link from 'next/link'
+import Navbar from "../components/navbar"
 
 export default function IndexPage() {
   return (
     <div>
+      <Navbar />
       Hello World.{' '}
       <Link href="/about">
         <a>About</a>
